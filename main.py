232da0f70e1e18a42a7aefceb6cb30c8b72ff6f6
RichardRivero <Richard_armando@hotmail.com>
2021-02-14 14:58:20 -0400

    agregando rutas

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask  # importamos la libreria
+from flask import Flask ,render_template  # importamos la libreria
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -9,17 +9,31 @@ app.config["SQLALCHEMY_DATABASE_URI"]= "sqlite:////home/richard/proyectos_Pyhton
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#Nota: normalemnte estes codigo va dentro de otro archivo
 class Post(db.Model): # creando la tabal de la bse de datos 
     __tablename__ = "posts"
     id = db.Column(db.Integer, primary_key = True)
     titulo = db.Column(db.String, nullable = False)
     fecha = db.Column(db.DateTime, default = datetime.now)
     texto = db.Column(db.String, nullable = False)
-
 # Nota : luego de crear la estructura de la tabla abrimos una terminal y creamos al DB
 # escribimos en treminal pyhton3       .. dentro de pyhton escribimos
 # from main import db
 # db.create_all()            con esto abremos creado el archoivo de la bd
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+
+@app.route("/")
+def inicio():
+    post = Post.query.orden_by(Post.fecha.desc()).all()
+    return render_template("inicio.html" , posts = posts)
+
+@app.route("/agregar")
+def agregar():
+    return render_template(agregar.html)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
