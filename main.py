2bf14fb04eaded63998c4cc735484bb9753daafa
RichardRivero <Richard_armando@hotmail.com>
2021-02-14 13:42:34 -0400

    modificando ruta db

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config("SQLALCHEMY_DATABASE_URI") = "sqlite:/home/richard/proyectos Pyhton/Blocg_flask/blog.db
+app.config("SQLALCHEMY_DATABASE_URI") = "sqlite:/home/richard/proyectos_Pyhton/Blocg_flask/blog.db
 "
 ## se configuro la ruta de la base de datos 
 app.config("SQLALCHEMY_TRACK_MODIFICATIONS") = False
