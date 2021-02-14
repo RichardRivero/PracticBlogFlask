047f884022681ce4b3ff53471972ebbf23565dd7
RichardRivero <Richard_armando@hotmail.com>
2021-02-14 13:24:33 -0400

    modificando variable

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Post(db.Model):
     __tablename__ = "posts"
     id = db.Column(db.Integer, primary_key = True)
     titulo = db.Column(db.String, nullable = False)
-    fecha = db.Column(db.Datetime, default = datetime.now)
+    fecha = db.Column(db.DateTime, default = datetime.now)
     texto = db.Column(db.String, nullable = False)
 
 
