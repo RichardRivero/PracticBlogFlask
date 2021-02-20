177becaea2d8e8fa1f8885eb60faef428339a900
RichardRivero <Richard_armando@hotmail.com>
2021-02-20 12:29:04 -0400

    corrigiendo errores encontrados

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-# nota las configuraciond se guaran entre cochetes no parentesos por que es un array
-app.config["SQLALCHEMY_DATABASE_URI"]= "sqlite:////home/richard/proyectos_Pyhton/Blocg_flask/blog.db"
+# nota las configuracion SQL se guardan entre cochetes no parentesos por que es un array
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////home/richard/proyectos_Pyhton/Blocg_flask/blog.db"
 ## se configuro la ruta de la base de datos 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
@@ -13,10 +13,10 @@ db = SQLAlchemy(app)
 #Nota: normalemnte estes codigo va dentro de otro archivo
 class Post(db.Model): # creando la tabal de la bse de datos 
     __tablename__ = "posts"
-    id = db.Column(db.Integer, primary_key = True)
-    titulo = db.Column(db.String, nullable = False)
-    fecha = db.Column(db.DateTime, default = datetime.now)
-    texto = db.Column(db.String, nullable = False)
+    id      = db.Column(db.Integer, primary_key = True)
+    titulo  = db.Column(db.String, nullable = False)
+    fecha   = db.Column(db.DateTime, default = datetime.now)
+    texto   = db.Column(db.String, nullable = False)
 # Nota : luego de crear la estructura de la tabla abrimos una terminal y creamos al DB
 # escribimos en treminal pyhton3       .. dentro de pyhton escribimos
 # from main import db
@@ -31,7 +31,7 @@ def inicio():
 
 @app.route("/agregar")
 def agregar():
-    return render_template(agregar.html)
+    return render_template("agregar.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
