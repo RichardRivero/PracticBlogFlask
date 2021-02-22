efabda15e3c76736ca29ef8b579dfac3659cfa4c
RichardRivero <Richard_armando@hotmail.com>
2021-02-22 16:00:38 -0400

    Agregando una pagina

--- main.py
+++ main.py
@@ -33,6 +33,10 @@ def inicio():
 def agregar():
     return render_template("agregar.html")
 
+@app.route("/otros")
+def otros():
+    return render_template("otros.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
 
